d86cea3d766cbdc22e0473c3c1ff81fcb04e9ff2
alenprastyaa <1001cerita1001@gmail.com>
2025-12-29 17:43:04 +0700

    push

--- src/views/DataProduk/DataProduk.vue
+++ src/views/DataProduk/DataProduk.vue
@@ -19,7 +19,7 @@
                     </button>
 
                     <button @click="openUploadModal"
-                        class="btn-secondary-emerald flex items-center gap-2 px-4 py-2 rounded-lg transition">
+                        class="btn-secondary-emerald flex items-center gap-2 px-4 py-2 ed-lg transition">
                         <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24"
                             xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
@@ -29,8 +29,8 @@
                         Upload Excel
                     </button>
 
-                    <button @click="openCreateModal"
-                        class="btn-primary flex items-center gap-2 px-4 py-2 rounded-lg transition">
+                    <button @click="openCreateModal" style="color: black;"
+                        class="flex items-center gap-2 px-4 py-2 rounded-lg transition">
                         <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <path d="M10 4V16M4 10H16" stroke="currentColor" stroke-width="2" stroke-linecap="round" />
                         </svg>
@@ -267,8 +267,8 @@
                                 class="btn-cancel flex-1 px-4 py-2 rounded-lg transition">
                                 Batal
                             </button>
-                            <button type="submit" :disabled="isSubmitting"
-                                class="btn-primary flex-1 px-4 py-2 rounded-lg transition disabled:opacity-50">
+                            <button type="submit" :disabled="isSubmitting" style="color: black;"
+                                class="flex-1 px-4 py-2 rounded-lg transition disabled:opacity-50">
                                 {{ isSubmitting ? 'Menyimpan...' : 'Simpan' }}
                             </button>
                         </div>
